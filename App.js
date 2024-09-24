f5996c7022a9a91950c9f6159dbd244e669198c5
cko1103 <chloeko.work@gmail.com>
2024-09-24 10:01:42 -0400

    Added App.js and styles.css, modified index.html

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Portfolio</h1>
+      </header>
+      <section>
+        <h2>Projects</h2>
+        <p>Here are my awesome projects!</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
